f41cbc26b26c010a7d3de4a0e11179462c48f017
dozerozturk <ozturkozer@yahoo.com>
2023-01-02 06:12:14 -0600

    Update

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
@@ -1,7 +1,21 @@
 package com.cydeo.tests.day4_findElements_checkboxes_radio;
 
 public class T6_StaleElementReferenceException {
+
+    public static void main(String[] args) {
+
+        //TC #6: StaleElementReferenceException Task
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/abtest
+        //3- Locate “CYDEO” link, verify it is displayed.
+        //4- Refresh the page.
+        //5- Verify it is displayed, again.
+
+
+    }
 }
+
+
 
 
 //3-39-21
