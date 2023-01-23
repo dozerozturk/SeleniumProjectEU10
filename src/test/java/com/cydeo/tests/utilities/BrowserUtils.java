d96d8ee8232584769d12f03a0741204a7c14d67c
dozerozturk <ozturkozer@yahoo.com>
2023-01-22 18:03:12 -0600

    Update

--- src/test/java/com/cydeo/tests/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/tests/utilities/BrowserUtils.java
@@ -5,12 +5,25 @@ In this class only general utility methods that are NOT related to some specific
  */
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 import java.util.Set;
 
 public class BrowserUtils {
+
+        /*
+    1. Create a new class called BrowserUtils
+    2. Create a method to make Task1 logic re-usable
+    3. When method is called, it should switch window and verify title.
+    Method info:
+            • Name: switchWindowAndVerify
+            • Return type: void
+            • Arg1: WebDriver
+            • Arg2: String expectedInUrl
+            • Arg3: String expectedTitle
+     */
+
+
 
     /*
     This method will accept int (in seconds) and execute Thread.sleep
@@ -49,7 +62,7 @@ public class BrowserUtils {
 
         //5. Assert:Title contains “expectedInTitle”
         String actualTitle = driver.getTitle();
-        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+        Assert.assertTrue(actualTitle.contains(expectedInTitle), "Actual title is not match");
     }
 
     /*
@@ -61,14 +74,17 @@ public class BrowserUtils {
 
     }
 
+
+
+
     /*
     Creating a utility method for ExplicitWait, so we don't have to repeat the lines
      */
-    public static void waitForInvisibilityOf(WebElement webElement){
-        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+//    public static void waitForInvisibilityOf(WebElement webElement){
+//        Driver.getDriver().manage().timeouts().implicitlyWait(0,TimeUnit.SECONDS);
 //        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10); //remove
 //        wait.until(ExpectedConditions.invisibilityOf(webElement)); //remove
-    }
+//    }
 
 
 }
